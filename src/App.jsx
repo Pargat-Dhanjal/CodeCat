9dedde2d768394a24d931b797450463ef2a27119
Pargat-Dhanjal <pargat14@hotmail.com>
2023-04-27 04:54:20 +0530

    feat: web analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { SnackbarProvider } from 'notistack';
 import { useState } from 'react';
 import router from './config/router';
 import { ThemeContext } from './contexts/ThemeContext';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   const [theme, setTheme] = useState(true);
@@ -12,13 +13,14 @@ function App() {
     setTheme(!theme);
   }
 
-  document.body.style.backgroundColor = theme? '#011627' : '#e0e0e0';
+  document.body.style.backgroundColor = theme ? '#011627' : '#e0e0e0';
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <SnackbarProvider maxSnack={3} autoHideDuration={2000}>
         <div className={theme ? 'dark' : 'light'}>
           <RouterProvider router={router} />
+          <Analytics />
         </div>
       </SnackbarProvider>
     </ThemeContext.Provider>
