58ef12b294f6c72ad5075eef00ad2e3788b03833
Pargat-Dhanjal <pargat14@hotmail.com>
2023-04-27 14:21:31 +0530

    feat: logout functionality

--- src/components/Compiler/Header.jsx
+++ src/components/Compiler/Header.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from 'react';
 import LanguagesDropdown from './LanguagesDropdown';
 import { ThemeContext } from '../../contexts/ThemeContext';
 import { RiSunFill, RiMoonFill } from 'react-icons/ri';
+import { MdLogout } from 'react-icons/md';
+import { logout } from '../../config/firebase';
 
 function Header({ handleLanguage }) {
   const { theme, toggleTheme } = useContext(ThemeContext);
@@ -15,10 +17,22 @@ function Header({ handleLanguage }) {
         style={{ fill: theme ? 'white' : 'black' }}
       />
       <div className="header-buttons">
-        <button className="dark-mode-button" onClick={toggleTheme}>
-          {theme ? <RiSunFill style={{ fill: 'white' }} size={30} /> : <RiMoonFill style={{ fill: 'black' }} size={30} />}
+        <button className="dark-mode-button" onClick={toggleTheme} title="Switch theme">
+          {theme ? (
+            <RiSunFill style={{ fill: 'white' }} size={30} />
+          ) : (
+            <RiMoonFill style={{ fill: 'black' }} size={30} />
+          )}
         </button>
         <LanguagesDropdown handleLanguage={handleLanguage} />
+        <button
+          className="dark-mode-button"
+          title="Log-out"
+          onClick={logout}
+          style={{ marginRight: 0, marginLeft: '1rem' }}
+        >
+          <MdLogout style={{ fill: 'crimson' }} size={30} />
+        </button>
       </div>
     </div>
   );
