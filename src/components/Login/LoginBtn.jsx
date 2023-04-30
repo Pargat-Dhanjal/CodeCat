49d4c4aeccccfccceee7238239d6fd24d543de6e
Pargat-Dhanjal <pargat14@hotmail.com>
2023-04-30 22:52:12 +0530

    feat: alignment, logout btn

--- src/components/Login/LoginBtn.jsx
+++ src/components/Login/LoginBtn.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function LoginBtn({text , onClick}) {
+function LoginBtn({text , onClick, styles}) {
   return (
-    <button onClick={onClick} className='login-btn'>
+    <button onClick={onClick} className='login-btn' style={styles}>
         {text}
     </button>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Profile from '../components/Home/Profile';
+import LoginBtn from '../components/Login/LoginBtn';
 
 function Home() {
   return (
@@ -16,8 +17,9 @@ function Home() {
         className="wrapper"
         style={{ flexDirection: 'row', padding: '1rem' }}
       >
-        <div className="wrapper" style={{ width: '30%' , marginRight :'1rem'}}>
+        <div className="wrapper" style={{ width: '30%' , marginRight :'1rem', flexDirection : 'column', justifyContent: 'start'}}>
           <Profile />
+          <LoginBtn text="Logout" styles={{backgroundColor: '#8F43EE' , color : '#fff'}} />
         </div>
         <div
           className="wrapper card"
