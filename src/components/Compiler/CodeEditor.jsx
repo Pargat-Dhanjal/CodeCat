6f1e4b7de4cbdb3abce40292d541925165e409d1
Pargat-Dhanjal <pargat14@hotmail.com>
2023-07-11 12:31:04 +0530

    feat: monaco base editor added

--- src/components/Compiler/CodeEditor.jsx
+++ src/components/Compiler/CodeEditor.jsx
@@ -1,7 +1,21 @@
 import { Box } from '@mui/material';
 import React from 'react';
+import { Editor, loader } from '@monaco-editor/react';
+import { dark } from '../../constants/themeDark';
 
 function CodeEditor() {
+  loader.init().then((monaco) => {
+    monaco.editor.defineTheme('dark', dark);
+  });
+
+  const [code, setCode] = React.useState('//Write your code here...');
+
+  // handle change
+  const handelChange = (value, event) => {
+    console.log(value);
+    setCode(value);
+  };
+
   return (
     <Box
       sx={{
@@ -9,10 +23,27 @@ function CodeEditor() {
         height: '100%',
         bgcolor: '#2B3245',
         padding: '0.7rem',
-        borderRadius: '20px'
+        borderRadius: 3,
       }}
     >
-        CodeEditor
+      <Box
+        sx={{
+          width: '100%',
+          height: '100%',
+          borderRadius: 2,
+          overflow: 'clip',
+        }}
+      >
+        <Editor
+          height="100%"
+          width="100%"
+          theme="dark"
+          language={'java'}
+          defaultValue="//Write your code here..."
+          value={code}
+          onChange={handelChange}
+        />
+      </Box>
     </Box>
   );
 }
